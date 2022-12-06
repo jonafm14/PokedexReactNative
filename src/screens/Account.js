7c672a5a3b66fe30a07966f452fdba7b208c87d3
Jonatan Musciachio <100133245+jonafm14@users.noreply.github.com>
2022-12-06 16:48:46 -0300

    Datos de usuarios y logout

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { View } from "react-native";
+import LoginForm from "../components/Auth/LoginForm";
+import UserData from "../components/Auth/UserData";
+import useAuth from "../hooks/useAuth";
 
 export default function Account() {
-  return (
-    <SafeAreaView>
-      <Text>Account</Text>
-    </SafeAreaView>
-  );
+  const { auth } = useAuth();
+
+  return <View>{auth ? <UserData /> : <LoginForm />}</View>;
 }
---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: "jonafm14",
+  password: "123456",
+};
+
+export const userDetails = {
+  username: "jonafm14",
+  firstName: "Jonatan",
+  lastName: "Musciachio",
+  email: "ejemplo@gmail.com",
+};
